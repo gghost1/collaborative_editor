b30acb04d220762e2880df1ef046b75c0e9b5a09
GG_Host <gd.host@yandex.ru>
2025-04-16 13:20:31 +0300

    feat: init structure

---
+++ backend/src/main/java/ru/collaborative_editor/CollaborativeEditorApplication.java
@@ -0,0 +1,13 @@
+package ru.collaborative_editor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborativeEditorApplication.class, args);
+	}
+
+}
